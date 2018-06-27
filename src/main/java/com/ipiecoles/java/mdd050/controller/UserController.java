cad2580945361c7704e795f5a121642bd7ee4c69
cedricBerth <cedric.berthezene@cgi.com>
2018-06-27 12:26:41 +0200

    Gestion des erreurs 404

--- src/main/java/com/ipiecoles/java/mdd050/controller/UserController.java
+++ src/main/java/com/ipiecoles/java/mdd050/controller/UserController.java
@@ -1,13 +1,16 @@
 package com.ipiecoles.java.mdd050.controller;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.ipiecoles.java.mdd050.exception.EmployeException;
 import com.ipiecoles.java.mdd050.model.Employe;
 import com.ipiecoles.java.mdd050.service.EmployeService;
 
@@ -18,19 +21,38 @@ public class UserController {
 	@Autowired
 	private EmployeService employeService;
 	
+	// Levée d'exception si id not found
+	private final static String EMP_NOT_FOUND = "Employé non trouvé"; 
+	
+	
 	@GetMapping("/count")
 	public Long countEmployes() {
 		return employeService.countAllEmploye(); 
 	}
 	
-	@GetMapping("/{id}")
-	public ResponseEntity<?> getEmploye(@PathVariable("id") Long id) {
+	@GetMapping("/details/{id}")
+	public Employe getEmploye(@PathVariable("id") Long id) throws EmployeException {
 		Employe empSearch = employeService.findById(id); 
 		if (empSearch == null || id == 0) {
-			return new ResponseEntity<String>("Employé inconnu", HttpStatus.NOT_FOUND); 
+			throw new EntityNotFoundException(EMP_NOT_FOUND); 
 		}
-		return new ResponseEntity<Employe>(empSearch, HttpStatus.ACCEPTED); 
+		return empSearch; 
 	}
+	
+	@SuppressWarnings("null")
+	@RequestMapping(
+		params = {"matricule"}, 
+		method = RequestMethod.GET
+	)
+	public Employe searchByMatricule(@RequestParam("matricule") String matricule) throws EmployeException{
+		Employe empSearch = employeService.findMyMatricule(matricule); 
+		if (empSearch == null) {
+			throw new EntityNotFoundException(EMP_NOT_FOUND); 
+		}
+		return empSearch; 
+	}
+	
+	
 	
 	
 
--- src/main/java/com/ipiecoles/java/mdd050/exception/EmployeException.java
+++ src/main/java/com/ipiecoles/java/mdd050/exception/EmployeException.java
@@ -1,10 +1,14 @@
 package com.ipiecoles.java.mdd050.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
 import com.ipiecoles.java.mdd050.model.Employe;
 
+@ResponseStatus(value=HttpStatus.NOT_FOUND)
 public class EmployeException extends Throwable {
     public static final String ID = "L'identifiant passé ne correspond pas à l'identifiant de l'employé : ";
-
+    
     public EmployeException(String message, Employe employe, Object valeurIncorrecte) {
         super(message + valeurIncorrecte + ", employe : " + employe.toString());
         System.out.println(this.getMessage());
